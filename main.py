661f8e065f64e7309dacc5e72de88d7ebdf27219
b2bagency-DK <rolf@b2b.agency>
2025-09-18 14:39:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import os
+os.environ["FLASK_APP"] = "main.py"
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hej fra min online server!"
+    return "Hej fra min online server! 🎉"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+
